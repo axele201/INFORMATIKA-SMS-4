82298dba44db84cd7d62135a6c40b7b094005d77
axele201 <148735797+axele201@users.noreply.github.com>
2025-03-18 18:11:42 +0700

    Update footer.blade.php

--- resources/views/LAYOUT/footer.blade.php
+++ resources/views/LAYOUT/footer.blade.php
@@ -1,5 +1,5 @@
 {{-- SCRIPT --}}
-<script src="{{asset('SCRIPT/script.js')}}"></script>
+<script src="{{asset('SCRIPT/js.js')}}"></script>
 {{-- ICONICONS --}}
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
