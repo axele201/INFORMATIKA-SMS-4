49a338181cd77a72be06f2729598cb823885db76
axele201 <148735797+axele201@users.noreply.github.com>
2025-03-20 20:04:58 +0700

    Update navbar.blade.php

--- resources/views/LAYOUT/navbar.blade.php
+++ resources/views/LAYOUT/navbar.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Schedule TI link -->
         <li class="navbar-item">
-            <button class="navbar-link" data-nav-link>SCHEDULE-Ti</button>
+            <button class="navbar-link" data-nav-link>Ti</button>
         </li>
 
         <!-- Timeline Link -->
