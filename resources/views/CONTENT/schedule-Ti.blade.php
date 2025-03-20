a0c086db6e769820e5a4457195dcf97c3f8fe0ac
axele201 <148735797+axele201@users.noreply.github.com>
2025-03-20 20:00:02 +0700

    Add files via upload

---
+++ resources/views/CONTENT/schedule-Ti.blade.php
@@ -0,0 +1,183 @@
+<article class="schedule-Ti" data-page="schedule-Ti">
+    <header>
+        <h2 class="h2 article-title">SCHEDULE</h2>
+    </header>
+
+    <section class="time">
+        <h3 class="h3 skills-title">Schedule</h3>
+        <div class="content-card" style="text-align: center;">
+            <div id="current-time" style="color: white"></div>
+            <h3 class="h3 skills-title">SISA WAKTU MENUJU KELAS BERIKUTNYA</h3>
+            <div id="next-class" style="color: white"></div>
+            <div id="countdown" style="color: white"></div>
+        </div>
+    </section>
+
+    <section class="timeline">
+        <div class="title-wrapper">
+            <div class="icon-box">
+                <ion-icon name="book-outline"></ion-icon>
+            </div>
+            <h3 class="h3">SENIN</h3>
+        </div>
+        <ol class="timeline-list">
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Information Technology Ethic, Regulation & Cyber Law</h4>
+                <span>17:00 - 19:00</span>
+                <p class="timeline-text">Dr. Edi Sofwan, S.H., M.H</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/85702053128?pwd=qgnjwklVcWFuvHtTBavCSbZck78b3A.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black"
+                                d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Big Data & Analytic</h4>
+                <span>19:00 - 21:00</span>
+                <p class="timeline-text">Ir. Onno W. Purbo, M.Eng., Ph.D.</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/82693444830?pwd=HkKoGgwhD3LNrRQhMkbgFgYQ0pssFW.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black"
+                                d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+        </ol>
+    </section>
+
+    <section class="timeline">
+        <div class="title-wrapper">
+            <div class="icon-box">
+                <ion-icon name="book-outline"></ion-icon>
+            </div>
+            <h3 class="h3">SELASA</h3>
+        </div>
+        <ol class="timeline-list">
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Komunikasi Data</h4>
+                <span>19:00 - 21:00</span>
+                <p class="timeline-text">Junaidi, S,Kom., M.Kom</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/84352033482?pwd=4ON90D4usfYlnujwo6F0RbuTxbC35r.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black"
+                                d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+        </ol>
+    </section>
+
+    <section class="timeline">
+        <div class="title-wrapper">
+            <div class="icon-box">
+                <ion-icon name="book-outline"></ion-icon>
+            </div>
+            <h3 class="h3">RABU</h3>
+        </div>
+
+        <ol class="timeline-list">
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Data Warehouse & Business Intelligence</h4>
+                <span>17:00 - 19:00</span>
+                <p class="timeline-text">Muhamad Yusuf, S.Kom., M.Kom</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/84537525255?pwd=ICKZkhF1YZk6K6I8CJg5xntuOujZS3.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black" d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+        </ol>
+    </section>
+
+    <section class="timeline">
+        <div class="title-wrapper">
+            <div class="icon-box">
+                <ion-icon name="book-outline"></ion-icon>
+            </div>
+            <h3 class="h3">KAMIS</h3>
+        </div>
+
+        <ol class="timeline-list">
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Cloud Computing</h4>
+                <span>19:00 - 21:00</span>
+                <p class="timeline-text">Aolia Ikhwanudin, S.Kom., M.Kom</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/86488868883?pwd=aanVcNFPXFla2Q5G2GKUpfXkWiQ4YY.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black" d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Rekayasa Perangkat Lunak</h4>
+                <span>19:00 - 21:00</span>
+                <p class="timeline-text">Luthfia Saskia</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/84485571990?pwd=vxMqbaNYQmGvBp28YtdMH0BpWyab2U.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black" d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+        </ol>
+    </section>
+    <section class="timeline">
+        <div class="title-wrapper">
+            <div class="icon-box">
+                <ion-icon name="book-outline"></ion-icon>
+            </div>
+            <h3 class="h3">JUMAT</h3>
+        </div>
+
+        <ol class="timeline-list">
+            <li class="timeline-item">
+                <h4 class="h4 timeline-item-title">Teknik Basis Data</h4>
+                <span>19:00 - 21:00</span>
+                <p class="timeline-text">Anas Nasrulloh, S.Kom., M.Kom.</p>
+                <p class="timeline-text">ZOOM</p>
+                <a href="https://us06web.zoom.us/j/81510476531?pwd=bbn05nJDPmInRIq5rTjhzjLNaywSfd.1">
+                    <button class="button-zoom">
+                        <div class="dots_border"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="sparkle">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke="black" fill="black" d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"></path>
+                        </svg>
+                        <span class="text_button">join now</span>
+                    </button>
+                </a>
+            </li>
+            <li></li>
+        </ol>
+    </section>
+
+</article>
